5b3ac81efe1de723b8c3bdb34badc0b1700fefc0
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2022-05-12 16:37:04 -0400

    server folder cleaned up. needs to connect data to DB and setup schema

--- server/index.js
+++ server/index.js
@@ -55,6 +55,8 @@ app.all("*", (req, res) => {
   }
 });
 
+app.use(errorHandler);
+
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
